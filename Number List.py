c8abc298e5b4db3ff6ac7342803992ad2b887407
FLUKE <paraminbrr2@gmail.com>
2018-07-13 11:28:32 +0700

    ADD file

---
+++ Number List.py
@@ -0,0 +1,14 @@
+"""
+Pre-Programming 2018 Onsite - Day:14
+Day:12/07/2561 Name: Number List
+"""
+def main():
+    """No.1 function"""
+    num = int(input())
+    text = []
+    for _ in range(num):
+        in_num = int(input())
+        text.append(in_num)
+    print(text)
+
+main()
